69a2df0698430f725aa1f170db5918773a6afdfc
Leonardo Alves de Paula e Silva <leoapsilva@gmail.com>
2020-11-22 01:32:51 -0300

    Setup DoctorController

--- app/Http/Controllers/DoctorController.php
+++ app/Http/Controllers/DoctorController.php
@@ -2,9 +2,16 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Doctor;
 use Illuminate\Http\Request;
 
 class DoctorController extends Controller
 {
     //
+    public function show($id)
+    {
+        $doctor = Doctor::find($id);
+
+        return view('doctors.show', ['doctor' => $doctor]);
+    }
 }
