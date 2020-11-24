cec38a1156c8f4aeb297ff8bb388e6bbf2c93979
Leonardo Alves de Paula e Silva <leoapsilva@gmail.com>
2020-11-24 20:33:27 -0300

    Fiz absence of empty on appointed datetime

--- resources/views/appointments/appointed-datetime.blade.php
+++ resources/views/appointments/appointed-datetime.blade.php
@@ -6,7 +6,7 @@
                 <div class="row">
                     <div class="col-md-9 {{ $errors->first('appointed_to') ? 'form-group has-error' : ''}}">
                         <input type="datetime-local" id="appointed_to" name="appointed_to" class="form-control"
-                            placeholder="" value="{{ Carbon\Carbon::parse($appointment->appointed_to)->format("Y-m-d\TH:i") }}" size="10">
+                            placeholder="" value="{{ !empty($appointment) ? Carbon\Carbon::parse($appointment->appointed_to)->format("Y-m-d\TH:i") : ''}}" size="10">
                     </div>
                 </div>
             </div>
