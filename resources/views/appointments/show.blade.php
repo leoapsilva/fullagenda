a572d542dcc36fb0e800f15b365a0854fe733e3f
Leonardo Alves de Paula e Silva <leoapsilva@gmail.com>
2020-12-08 11:31:37 -0300

    Corrigindo erro na visualização de agendamento issue #23

--- resources/views/appointments/show.blade.php
+++ resources/views/appointments/show.blade.php
@@ -5,54 +5,85 @@
 @endphp 
 
 @section('content')
-
     <div class="row">
         <div class="col-md-12">
             <div class="panel panel-default">
                 <div class="panel-heading">
-                {{ __("navbar.".$nav) }}
-                    <span class="pull-right clickable panel-toggle panel-button-tab-left"><em class="fa fa-toggle-up"></em></span>
+                {{ __("Alterar agendamento") }}
                 </div>
                 <div class="panel-body">
 
-                    <form class="form-horizontal" action="/doctors" method="GET">
+                    <form class="form-horizontal" action="/appointments/{{ $appointment->id}}" method="POST">
+                        @csrf
+                        @method('PUT')
                         <fieldset>
-                            <!-- Name input-->
-                            <div class="form-group">
-                                <label class="col-md-3 control-label" for="name"> {{ __('doctors.name') }} </label>
-                                <div class="col-md-9">
-                                    <input disabled id="name" name="name" type="text" placeholder="" class="form-control" value="{{ $doctor->name }}">
-                                </div>
-                            </div>
-                        
-                            <!-- Lastname input-->
-                            <div class="form-group">
-                                <label class="col-md-3 control-label" for="lastname">{{ __('doctors.lastname') }} </label>
-                                <div class="col-md-9">
-                                    <input disabled id="lastname" name="lastname" type="text" placeholder="" class="form-control" value="{{ $doctor->lastname }}">
-                                </div>
-                            </div>
-                            
-                            <!-- Mobile input -->
-                            <div class="form-group">
-                                <label class="col-md-3 control-label" for="mobile">{{ __('doctors.mobile') }} </label>
-                                <div class="col-md-9">
-                                    <input disabled type="tel" id="mobile" name="mobile" class="form-control" placeholder="" value="{{ $doctor->mobile }}" >
-                                </div>
-                            </div>
 
-                            <!-- Specialty input-->
-                            <div class="form-group">
-                                <label class="col-md-3 control-label" for="specialty">{{ __('doctors.specialty') }} </label>
-                                <div class="col-md-9">
-                                    <input disabled id="specialty" name="specialty" type="text" placeholder="" class="form-control" value="{{ $doctor->specialty }}">
-                                </div>
-                            </div>
+                            <div class="row md-12">
+
+                                <!-- Doctor input-->
+                                {{-- <div class="form-group"> --}}
+                                    <div class="col-md-4">
+                                        <div class="panel panel-default">
+                                            <div class="panel-heading text-center">
+                                                Médico
+                                            </div>
+                                            <div class="panel-body text-center">
+                                                {{ $appointment->doctor->name }} {{ $appointment->doctor->lastname }}
+                                            </div>
+                                            <div class="panel-body text-center">
+                                                {{ $appointment->doctor->specialty }}
+                                            </div>
+                                        </div>
+                                    </div>
+                                {{-- </div> --}}
+
+                                <!-- Appointed To input-->
+                                {{-- <div class="form-group"> --}}
+                                    <div class="col-md-4">
+                                        <div class="panel panel-default">
+                                            <div class="panel-heading text-center">
+                                                Agendado para
+                                            </div>
+                                            <div class="panel-body text-center">
+                                                {{ Carbon\Carbon::parse($appointment->appointed_to)->format("d/m/Y H:i") }} 
+                                            </div>
+                                        </div>
+                                    </div>
+                                {{-- </div> --}}
+
+                                <!-- Patient input-->
+                                {{-- <div class="form-group"> --}}
+                                    <div class="col-md-4">
+                                        <div class="panel panel-default">
+                                            <div class="panel-heading text-center">
+                                                Paciente
+                                            </div>
+                                            <div class="panel-body text-center">
+                                                {{ $appointment->patient->name }} {{  $appointment->patient->lastname }}
+                                            </div>
+                                            <div class="panel-body text-center">
+                                                {{ $appointment->patient->mobile }}
+                                            </div>
+                                            <div class="panel-body text-center">
+                                                {{ $appointment->patient->health_insurance_plan }}
+                                            </div>
+                                        </div>
+                                    </div>
+                                {{-- </div> --}}
+
+                            </div><!--/.row-->
                             
+                            <input type="hidden" name="user_id" id="user_id" value="{{ Auth::user()->id}}">
+
                             <!-- Form actions -->
                             <div class="form-group">
                                 <div class="col-md-12 widget-right">
-                                    <button type="submit" class="btn btn-warning btn-md pull-right">Voltar</button>
+                                    <div class="col-md-10 widget-left">
+                                    </div>
+                                    <div class="col-md-1 widget-right">
+                                        <button type="button" class="btn btn-warning btn-md pull-left" onclick="location.href='/appointments'">Voltar</button>
+                                    </div>
+                                    <button type="button" class="btn btn-success btn-md pull-right" onclick="location.href='/appointments/{{ $appointment->id }}/edit'">Alterar</button>
                                 </div>
                             </div>
                         </fieldset>
@@ -65,4 +96,5 @@
 @endsection
 
 @section('extra_js')
+
 @endsection
